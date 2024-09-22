b9c6cb12c2982c2f3742f78ed4e46670b28b559a
Y79S84M <yostm.dev@gmail.com>
2024-09-22 22:19:05 +0200

    fine tuning hero-banner

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 const TOGGLE_BUTTON = window.document.getElementById('toggle-menu');
-const NAV_LINKS = window.document.getElementById('nav-links');
+const NAV_LINKS = window.document.getElementById('nav-links-container');
+const HERO_LOGO = window.document.getElementById('hero-logo');
 
 TOGGLE_BUTTON.addEventListener('click',toggleMenu);
 
 function toggleMenu(){
+    
     NAV_LINKS.classList.toggle('hidden');
+
 }
